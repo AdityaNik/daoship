86fc8364018b6793822aacd35415e2f1b25b7c3c
ditya <105343290+AdityaNik@users.noreply.github.com>
2025-09-23 19:17:54 +0530

    explore page done

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,3 +1,4 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 
 export default function ExplorePage() {
@@ -8,7 +9,23 @@ export default function ExplorePage() {
         Discover and join DAO's created with DAOShip{" "}
       </span>
       <div className="mt-10">
-        <Input placeholder="Search DAO" />
+        <Input className="p-6" placeholder="Search DAO" />
+      </div>
+
+      <div className="flex gap-16 flex-wrap mt-12">
+        <Card>
+          <CardHeader>
+            <img src="/temp.png" alt="DAO Logo" className="" />
+          </CardHeader>
+          <CardContent>
+            <div className="mt-2">Swap Platform</div>
+            <div>Platform to swap stable and unstable coins</div>
+            <div className="mt-10 flex justify-between">
+              <div>2 members</div>
+              <div>created 23/9/2025</div>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
